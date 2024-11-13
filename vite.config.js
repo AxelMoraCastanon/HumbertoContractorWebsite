90613419ad8767109bb45b8d7035406e74f5c2eb
Axel Mora <axelmora2011@gmail.com>
2024-11-12 18:22:24 -0800

    Fixing Website to reroute to vanguarddevelopmentsinc.com

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HumbertoContractorWebsite/', 
+  base: '/',  // Custom domain requires base set to '/'
   plugins: [react()],
 })
